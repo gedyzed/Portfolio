dd549969c1693fe280418ac98caf84eed0d0806d
Geda <gediizeyuu@gmail.com>
2024-12-22 11:18:30 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="content">
+        <h1 className="title">My Portfolio</h1>
+        <p className="subtitle">is being prepared</p>
+        <div className="loader">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
